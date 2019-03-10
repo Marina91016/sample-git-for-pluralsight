aa22749179e07ed07895bd00aa4b99b546e98294
Marina <marinashister24@gmail.com>
2019-03-10 15:43:37 +0200

    Update the toString method

--- src/org/paumard/model/User.java
+++ src/org/paumard/model/User.java
@@ -12,12 +12,7 @@ public class User {
     public void setName(String name) {
         this.name = name;
     }
-
-    @Override
-    public String toString() {
-        return "User [name=" + name + "]";
-    }
-
+    
     public int getAge() {
         return age;
     }
@@ -25,5 +20,9 @@ public class User {
     public void setAge(int age) {
         this.age = age;
     }
-    
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", age=" + age + "]";
+    }
 }
